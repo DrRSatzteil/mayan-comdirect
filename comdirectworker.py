4cc8ee7001f7263b8c1bfe0f3c5150aef5a87c78
DrRSatzteil <lauterbachthomas@gmail.com>
2021-07-04 23:31:35 +0200

    made config for matching more flexible

--- comdirectworker.py
+++ comdirectworker.py
@@ -1,12 +1,16 @@
+from babel import numbers
 from datetime import datetime
 from logging.config import fileConfig
 from typing import Dict, Type
 import comdirect
+import decimal
 import json
+import locale
 import logging
 import mayan
 import os
 import pickle
+import re
 import redis
 
 
@@ -31,6 +35,18 @@ def get_options():
     return config
 
 
+def get_config():
+    matching = json.load(open('/app/config/matching.json',))
+    mapping = json.load(open('/app/config/mapping.json',))
+    tagging = json.load(open('/app/config/tagging.json',))
+    config = {
+        "matching": matching,
+        "mapping": mapping,
+        "tagging": tagging
+    }
+    return config
+
+
 def get_mayan(args):
     _logger.info("logging into mayan")
     m = mayan.Mayan(args["url"])
@@ -52,22 +68,14 @@ def get_comdirect(args):
 
 def single(document):
     args = get_options()
-    f1 = open('/app/config/matching.json',)
-    f2 = open('/app/config/mapping.json',)
-    f3 = open('/app/config/tagging.json',)
-    matching = json.load(f1)
-    mapping = json.load(f2)
-    tagging = json.load(f3)
-    _logger.debug('Loaded matching: ' + str(matching))
-    _logger.debug('Loaded mapping: ' + str(mapping))
-    _logger.debug('Loaded tagging: ' + str(tagging))
+    config = get_config()
     m = get_mayan(args)
     c = get_comdirect(args)
     _logger.info("load document %s", document)
-    process(m, c, document, matching, mapping, tagging)
+    process(m, c, document, config)
 
 
-def process(m, c, document, matching, mapping, tagging):
+def process(m, c, document, config):
     if isinstance(document, str):
         if document.isnumeric():
             document = m.get(m.ep(f"documents/{document}"))
@@ -85,41 +93,59 @@ def process(m, c, document, matching, mapping, tagging):
     }
 
     search_criteria = {}
+    unsigned = True
     try:
-        search_criteria['invoice_amount'] = doc_metadata[matching['invoice_amount']]['value']
-        search_criteria['invoice_number'] = doc_metadata[matching['invoice_number']]['value']
-        search_criteria['invoice_date'] = doc_metadata[matching['invoice_date']]['value']
+        amount = doc_metadata[config['matching']
+                              ['invoice_amount']['metadatatype']]['value']
+        unsigned = config['matching']['invoice_amount']['unsigned']
+        amountlocale = config['matching']['invoice_amount']['locale']
+        amount_filtered = ''.join(str(c) for c in (
+            list(filter(lambda x: x in '-0123456789.,', amount))))
+        amount_decimal = numbers.parse_decimal(
+            amount_filtered, locale=amountlocale)
+        search_criteria['invoice_amount'] = amount_decimal
+
+        search_criteria['invoice_number'] = doc_metadata[config['matching']
+                                                         ['invoice_number']['metadatatype']]['value']
+
+        date = doc_metadata[config['matching']
+                            ['invoice_date']['metadatatype']]['value']
+        format = config['matching']['invoice_date']['dateformat']
+        search_criteria['invoice_date'] = datetime.strptime(date, format)
     except:
-        _logger.error('Not all required metadata was present')
+        _logger.error('Matching configuration is incomplete or incorrect.')
         raise
 
     _logger.debug('Document metadata found: ' + str(search_criteria))
 
-    # TODO: Support different date formats
     c.login()
-    try:
-        transactions = c.get_transactions(datetime.strptime(
-            search_criteria['invoice_date'], '%Y-%m-%d'))
-    except:
-        _logger.error('Expected %Y-%m%d date format but received: ' +
-                      search_criteria['invoice_date'])
-        raise
+    transactions = c.get_transactions(search_criteria['invoice_date'])
     cache_api_state(c)
 
     for tx in transactions:
         try:
             tx_amount = tx['amount']['value']
             tx_remittanceInfo = tx['remittanceInfo']
-            # TODO: Be more flexible regarding currency and metadata formatting...
-            if(tx_amount.replace('-', '') == search_criteria['invoice_amount'].replace('€', '').replace(',', '.') and search_criteria['invoice_number'] in tx_remittanceInfo):
+
+            if unsigned:
+                tx_amount = tx_amount.replace('-', '')
+
+            tx_amount_decimal = numbers.parse_decimal(
+                tx_amount, locale='en_US')
+
+            if(tx_amount_decimal == search_criteria['invoice_amount']
+                    and search_criteria['invoice_number'] in tx_remittanceInfo):
                 _logger.info('Found transaction for document ' + str(document))
                 metadata = {}
-                for property in mapping.keys():
+                # TODO: Add possibility to configure mappings on deeper levels
+                # and basic transformations e.g. for date formats
+                for property in config['mapping'].keys():
                     try:
                         propertyValue = tx[property]
-                        metadata[mapping[property]] = propertyValue
+                        metadata[config['mapping'][property]] = propertyValue
                     except:
-                        _logger.error('Property ' + property + ' not found in transaction.')
+                        _logger.error('Property ' + property +
+                                      ' not found in transaction.')
 
                 for meta in m.document_types[document["document_type"]["label"]]["metadatas"]:
                     meta_name = meta["metadata_type"]["name"]
@@ -142,23 +168,28 @@ def process(m, c, document, matching, mapping, tagging):
                             data = {"value": metadata[meta_name]}
                             result = m.put(
                                 m.ep(
-                                    "metadata/{}".format(doc_metadata[meta_name]["id"]),
+                                    "metadata/{}".format(
+                                        doc_metadata[meta_name]["id"]),
                                     base=document["url"],
                                 ),
                                 json_data=data,
                             )
-                    for t in tagging['tags']:
+                    for t in config['tagging']['tags']:
                         if t not in m.tags:
                             _logger.info("Tag %s not defined in system", t)
                             continue
                         data = {"tag_pk": m.tags[t]["id"]}
-                        result = m.post(m.ep("tags", base=document["url"]), json_data=data)
+                        result = m.post(
+                            m.ep("tags", base=document["url"]), json_data=data)
                 break
         except:
-            _logger.debug('No amount or remittanceInfo found. Skipping transaction.')
+            _logger.debug(
+                'No amount or remittanceInfo found. Skipping transaction.')
+            raise
 
 
 def cache_api_state(comdirect):
     pickled = pickle.dumps(comdirect)
-    # We need to log in again after 20 minutes anyway so we might as well clear the cache after 20 minutes
+    # We need to log in again after 20 minutes anyway
+    # so we might as well clear the cache after 20 minutes
     redis_conn.set('comdirect_cache', pickled, 1200)
